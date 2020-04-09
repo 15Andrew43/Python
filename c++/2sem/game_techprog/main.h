91aa3ef6250396fd4710854d01b0bf5a2ae5154a
Andrew <Borovets.AV@phystech.edu>
2020-04-09 20:48:24 +0300

    check

---
+++ c++/2sem/game_techprog/main.h
@@ -0,0 +1,11 @@
+//
+// Created by Андрей Боровец on 09.04.2020.
+//
+
+#ifndef GAME_TECHPROG_MAIN_H
+#define GAME_TECHPROG_MAIN_H
+
+#include "unist.h"
+#include "buildings.h"
+
+#endif //GAME_TECHPROG_MAIN_H
---
+++ c++/2sem/game_techprog/units/unit.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Андрей Боровец on 09.04.2020.
+//
+#include <iostream>
+#include "unit.h"
+
+do_smth() // обход карты, чтобы продамажить жизни
+
+void Swordsman::do() {
+
+    std::cout << "somth doing"
+}
